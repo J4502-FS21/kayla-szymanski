558a84411ade213c68644612569c689a84b7e1e4
kaylarose55 <krsq94@umsystem.edu>
2021-11-01 18:26:47 -0500

    javascript

---
+++ quiz.css
@@ -0,0 +1,106 @@
+#menu_nav {
+  background-color: #756D60;
+  font-family: 'Montserrat', sans-serif;
+  font-style: italic;
+  list-style-type: none;
+  margin: 0px;
+  padding: 10px;
+  display:flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items:stretch;
+}
+
+#quizWrap {
+  max-width: 600px;
+  margin: 0 auto;
+
+}
+
+/* (B) QUESTION */
+#quizQn {
+  padding: 20px;
+  background: #4c93ba;
+  color: #fff;
+  font-size: 24px;
+  border-radius: 10px;
+}
+body{
+  background: #EBDABF;
+  text-align: center;
+  margin: auto;
+  padding: auto;
+  font-family: 'Montserrat', sans-serif;
+}
+
+h1{
+  font-family:'Italiana', serif;
+  font-weight: 400;
+  margin: auto;
+  padding: auto;
+  font size: 16px
+}
+
+h2{
+  font-family:'Italiana', serif;
+  font-weight: 400;
+  font-size: 50px;
+  margin: 2px
+  padding: 2px
+}
+
+h3 {
+  font-family: 'Italiana', serif;
+  font-size: 1.25em;
+}
+
+ul{
+  margin: :10px
+  padding: 10px
+}
+
+li{
+  margin: 0px;
+  padding: 0px;
+  border: none;
+}
+
+p {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1em;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 10px;
+  max-width: 600px;
+  text-align: center;
+}
+/* (C) ANSWERS */
+#quizAns {
+  margin: 10px 0;
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-gap: 10px;
+}
+#quizAns input[type=radio] { display: none; }
+#quizAns label {
+  background: #CEBFA7;
+  border: 1px solid #eee;
+  border-radius: 10px;
+  padding: 10px;
+  font-size: 20px;
+  cursor: pointer;
+  text-align: center;
+}
+#quizAns label.correct {
+  background: #756D60;
+  border: 1px solid #60a03f;
+}
+#quizAns label.wrong {
+  background: #756D60;
+  border: 1px solid #white;
+}
+
+html, body {
+  background: #756D60;
+  font-family: Montserrat, sans-serif;
+}
